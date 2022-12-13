de170c88317368554d03b6a4c94b81b57085fab2
amadi5892 <amadi5892@gmail.com>
2022-12-13 16:01:05 -0500

    mapped project card

--- src/components/experience/projects/projects.component.jsx
+++ src/components/experience/projects/projects.component.jsx
@@ -7,16 +7,40 @@ import '../../../assets/grid/css/2cols.css'
 
 
 const Projects = () => {
+    const projs = [
+        {
+            id: 1,
+            image: 'weather-app',
+            title: 'Weather App',
+            url: 'https://frolicking-beignet-093c24.netlify.app/'
+        },
+        {
+            id: 2,
+            image: 'expense_tracker',
+            title: 'Expense Tracker',
+            url: 'https://sprightly-lebkuchen-6f5d49.netlify.app/'
+        },
+        {
+            id: 3,
+            image: 'to_do_list',
+            title: 'To-Do List',
+            url: 'https://bright-parfait-27c572.netlify.app/'
+        }
+        
+    ]
+
   return (
     <div className='projects-container' >
         <div className='projects-text-box span_1_of_2' >
             <h2>Check out my previous <span>projects</span></h2>
         </div>
         <div className='projects-card-container span_1_of_2' onClick={e => window.location.href='https://frolicking-beignet-093c24.netlify.app/'}>
-            <div className='projects-card span_1_of_2' >
-                <img className='card-img' src={weather} alt='weather app' />
-                <h4>Weather App</h4>
-            </div>
+            {projs.map((proj) => (
+                <div className='projects-card span_1_of_2' >
+                    <img className='card-img' src={require('../../../assets/img/'+proj.image+'.jpg')} alt='weather app' />
+                    <h4>{proj.title}</h4>
+                </div>
+            ))}
         </div>
     </div>
   )
